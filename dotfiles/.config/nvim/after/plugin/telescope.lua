a1cda835c3859a869696cce3a156767d4cbbd1ce
Chris Tapper <mail@cjtapper.com>
2023-05-13 09:10:07 +1000

    fix: make telescope use git_files for C-p

--- dotfiles/.config/nvim/after/plugin/telescope.lua
+++ dotfiles/.config/nvim/after/plugin/telescope.lua
@@ -14,7 +14,6 @@ telescope.setup{
 telescope.load_extension('fzf')
 
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<C-P>', builtin.find_files, {})
 vim.keymap.set('n', '<C-/>', builtin.live_grep, {})
 vim.keymap.set('n', '<C-_>', builtin.live_grep, {})
 vim.keymap.set('n', '<C-b>', builtin.buffers, {})
