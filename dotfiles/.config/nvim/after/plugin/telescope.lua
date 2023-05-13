f068de52dd7764d92fcb36efcc441850cee7ba39
Chris Tapper <mail@cjtapper.com>
2023-05-13 11:14:31 +1000

    Use git_files if in a git repo; else find_files

--- dotfiles/.config/nvim/after/plugin/telescope.lua
+++ dotfiles/.config/nvim/after/plugin/telescope.lua
@@ -13,7 +13,17 @@ telescope.setup{
 }
 telescope.load_extension('fzf')
 
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+-- Use git_files if in a git repo, 
+local project_files = function()
+  vim.fn.system('git rev-parse --is-inside-work-tree')
+  if vim.v.shell_error == 0 then
+    require"telescope.builtin".git_files()
+  else
+    require"telescope.builtin".find_files()
+  end
+end
+
+vim.keymap.set('n', '<C-p>', project_files, {})
 vim.keymap.set('n', '<C-/>', builtin.live_grep, {})
 vim.keymap.set('n', '<C-_>', builtin.live_grep, {})
 vim.keymap.set('n', '<C-b>', builtin.buffers, {})
