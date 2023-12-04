44f15d53da112d4f6c65d8c074f2016a7d67c679
Chris Tapper <mail@cjtapper.com>
2023-12-04 14:11:20 +1100

    Remove dependency on lsp-zero

--- dotfiles/.config/nvim/lua/plugins/lspconfig.lua
+++ dotfiles/.config/nvim/lua/plugins/lspconfig.lua
@@ -14,7 +14,6 @@ return  {
       },
     },
     config = function()
-      local lsp_zero = require('lsp-zero')
       local lspconfig = require('lspconfig')
 
       lspconfig.lua_ls.setup({
@@ -27,31 +26,19 @@ return  {
           }
         }
       })
+      -- Note: Plugins for black, isort, flake8 need to be installed in the same
+      -- venv as pyslp 
       lspconfig.pylsp.setup({
         settings = {
           pylsp = {
+            configurationSources = {'flake8'},
             plugins = {
-              black = {
-                enabled = true
-              },
-              flake8 = {
-                enabled = true
-              },
-              isort = {
-                enabled = true
-              },
-              mccabe = {
-                enabled = false
-              },
-              pyflakes = {
-                enabled = false
-              },
-              pycodestyle = {
-                enabled = false
-              },
-              rope_autoimport = {
-                enabled = true
-              }
+              black = { enabled = true },
+              flake8 = { enabled = true },
+              isort = { enabled = true },
+              mccabe = { enabled = false },
+              pyflakes = { enabled = false },
+              pycodestyle = { enabled = false },
             }
           },
         }
@@ -74,22 +61,24 @@ return  {
         virtual_text = true
       })
 
-      lsp_zero.on_attach(function(_, bufnr)
-        local opts = {buffer = bufnr, remap = false}
+      vim.api.nvim_create_autocmd('LspAttach', {
+        group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+        callback = function(ev)
+          local opts = {buffer = bufnr, remap = false}
 
-        vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-        vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-        vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-        vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-        vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-        vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-        vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-        vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-        vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-        vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-      end)
-
-      lsp_zero.setup()
+          vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+          vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+          vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+          vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+          vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+          vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+          vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+          vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+          vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+          vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+          vim.keymap.set("n", "<leader>vf", function() vim.lsp.buf.format() end, opts)
+        end,
+      })
     end
   }
 }
