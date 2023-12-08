0e12dd253e71f1f95a80b0345a177d2d95ff1125
Chris Tapper <mail@cjtapper.com>
2023-12-08 14:17:07 +1100

    Alias for git restore .

--- dotfiles/.config/fish/config.fish
+++ dotfiles/.config/fish/config.fish
@@ -31,6 +31,7 @@ abbr --add -- gaa git add --all
 abbr --add --set-cursor -- gcm git commit --message \"%\"
 abbr --add -- gds git diff --staged
 abbr --add --set-cursor -- gsc git switch --create \"%\"
+abbr --add -- gr. git restore .
 abbr --add -- gsm git switch "main"
 abbr --add -- gs- git switch -
 
