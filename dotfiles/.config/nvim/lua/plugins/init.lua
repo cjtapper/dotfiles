9911a0d10e3b7675dd9d7a3a6c19eb0f95439d8d
Chris Tapper <mail@cjtapper.com>
2024-11-08 10:47:08 +1100

    Tweaks

--- dotfiles/.config/nvim/lua/plugins/init.lua
+++ dotfiles/.config/nvim/lua/plugins/init.lua
@@ -4,7 +4,7 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
-      vim.g.everforest_enable_italic = true
+      vim.g.everforest_enable_italic = false
       vim.g.everforest_background = 'hard'
       vim.cmd.colorscheme('everforest')
     end
